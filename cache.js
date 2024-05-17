c3f53eaa0e78bec148007bb83a41b2d34c4975e1
Edgard <eins.yeager@gmail.com>
2024-05-17 14:26:47 +0500

    update

--- cache.js
+++ cache.js
@@ -97,3 +97,72 @@ objRef.go();               // (1) [object Object]
 const binded = objRef.go.bind({ name: 'Doe' });
 console.log(binded());
 // ---Refference type---
+
+// ---Promise all ---
+var promiseAll = function (functions) {
+	return new Promise((resolve, reject) => {
+		var l = []
+		var count = 0
+		for (var key in functions) {
+			(function (key) {
+				functions[key]().then(res => {
+					l[key] = res;
+					count++;
+					if (count === functions.length) {
+						resolve(l)
+					}
+				}).catch(rej => { reject(rej) })
+			})(key)
+		}
+	})
+};
+// ---Promise all---
+
+// ---Function generator---
+function* indexGenerator() {
+	let index = 0;
+	while (true) {
+		yield index++;
+	}
+}
+const g = indexGenerator();
+console.log(g.next().value); // => 0
+console.log(g.next().value); // => 1
+// ---Function generator---
+
+// ---Map---
+let map = new Map();
+
+map.set('1', 'str1');   // a string key
+map.set(1, 'num1');     // a numeric key
+map.set(true, 'bool1'); // a boolean key
+
+// remember the regular Object? it would convert keys to string
+// Map keeps the type, so these two are different:
+alert(map.get(1)); // 'num1'
+alert(map.get('1')); // 'str1'
+
+alert(map.size); // 3
+// ---Map---
+
+// ---Set---
+let set = new Set();
+
+let john = { name: "John" };
+let pete = { name: "Pete" };
+let mary = { name: "Mary" };
+
+// visits, some users come multiple times
+set.add(john);
+set.add(pete);
+set.add(mary);
+set.add(john);
+set.add(mary);
+
+// set keeps only unique values
+alert(set.size); // 3
+
+for (let user of set) {
+	alert(user.name); // John (then Pete and Mary)
+}
+// ---Set---
